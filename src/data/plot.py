652b8f78d874c66448d3b4c3d1eefb665db05c6c
annie-waye <anniewaye@gmail.com>
2018-11-05 12:59:16 -0500

    format improvements
    now rotating xlabels to better fit plot

--- src/data/plot.py
+++ src/data/plot.py
@@ -1,9 +1,21 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
-def primary_plot(dataframe, axis):
+
+def primary_plot(dataframe, axis, title=None):
+    """
+
+    :param dataframe:
+    :param axis:
+    :param title:
+    :return:
+    """
+
     ax = dataframe[axis].plot(kind='bar')
     xtl = [item.get_text()[:10] for item in ax.get_xticklabels()]
-    _ = ax.set_xticklabels(xtl)
+    # _ = ax.set_xticklabels(xtl)
+    plt.xticks(np.arange(0, len(dataframe[axis])), xtl, rotation=22.5)
+    plt.ylabel(axis)
+    plt.title(title)
     plt.tight_layout()
     plt.show()
